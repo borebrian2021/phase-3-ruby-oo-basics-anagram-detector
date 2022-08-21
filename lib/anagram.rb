7690021a11b5736fb80cdb2cd3711c08be4294b6
borebrian2021 <bkimutai2021@gmail.com>
2022-08-21 09:51:50 +0300

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        sorted = array.map do |word|
+            if word.chars.sort == @word.chars.sort
+                word
+            else nil
+            end
+        end
+        finded = sorted.filter do |word|
+            word != nil
+        end
+        finded
+    end
+end
+
+listen = Anagram.new("allergy")
+print listen.match(%w[gallery ballerina regally clergy largely leading])
